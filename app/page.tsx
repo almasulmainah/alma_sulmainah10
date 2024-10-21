0e40b8b8a39c6f5fd6532850a10b0684c5411534
Alma Sulmainah <152006793+almasulmainah@users.noreply.github.com>
2024-10-21 16:01:28 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import RowRiwayat from "./components/rowRiwayat";
+import RiwayatPendidikan from "./components/riwayatPendidikan";
+import Header from "./components/header";
+import RiwayatKerja from "./components/riwayatKerja";
+import ColorPicker from "./components/colorPicker";
+import foto from "./me.jpg";
+import "./alma.css";
+
+
+
+export default function Gallery() {
+  return (
+    <section>
+      <Header />
+      <RiwayatPendidikan />
+      <RiwayatKerja />
+      <ColorPicker />
+    </section>
+  );
+}
